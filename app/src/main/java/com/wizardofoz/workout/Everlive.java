93b23306e6f5b90a6060de1c242af9512eeb1f1a
ksevery <konstantinsevery@gmail.com>
2014-10-13 13:01:43 +0300

    Made a custom ListView adapter, cannot test on my device. Modified the Everlive class a bit, added a few string resources.

--- app/src/main/java/com/wizardofoz/workout/Everlive.java
+++ app/src/main/java/com/wizardofoz/workout/Everlive.java
@@ -3,18 +3,18 @@ package com.wizardofoz.workout;
 import com.telerik.everlive.sdk.core.EverliveApp;
 
 public class Everlive {
-    private EverliveApp app;
+    private static EverliveApp app;
     private static Everlive instance;
 
     protected Everlive(String apiKey){
         app = new EverliveApp(apiKey);
     }
 
-    public static Everlive getEverlive(){
+    public static EverliveApp getEverlive(){
         if (instance == null){
             instance = new Everlive("8hJ6nOSy4uST8iDq");
         }
 
-        return instance;
+        return app;
     }
 }
